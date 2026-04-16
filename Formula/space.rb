fb8d034641427d2676600f621bd2db804d4beb60
Dade Roode <68024680+daderoode@users.noreply.github.com>
2026-04-16 13:25:24 +0000

    space 0.6.2

--- Formula/space.rb
+++ Formula/space.rb
@@ -1,18 +1,18 @@
 class Space < Formula
   desc "Workspace manager for multi-repo git worktrees"
   homepage "https://github.com/daderoode/space"
-  version "0.6.1"
+  version "0.6.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/daderoode/space/releases/download/v0.6.1/space-v0.6.1-aarch64-apple-darwin.tar.gz"
-      sha256 "94b23bb13a6d306bca610cf8bd2817addb8fa781889ce0c38ff10c110b7dfd29"
+      url "https://github.com/daderoode/space/releases/download/v0.6.2/space-v0.6.2-aarch64-apple-darwin.tar.gz"
+      sha256 "44c047cad09d42fec350ef71fb228898ae1e2161167c62ce33d05348b7ca5d0b"
     end
 
     on_intel do
-      url "https://github.com/daderoode/space/releases/download/v0.6.1/space-v0.6.1-x86_64-apple-darwin.tar.gz"
-      sha256 "d064c53c50283faa918857099c7f0b3bec212aa364e4cec288f825153cffe442"
+      url "https://github.com/daderoode/space/releases/download/v0.6.2/space-v0.6.2-x86_64-apple-darwin.tar.gz"
+      sha256 "1381b3da3bc731f6a2c849bf6b11feeb475bdffcfe225becf3486fff5061c10a"
     end
   end
 
